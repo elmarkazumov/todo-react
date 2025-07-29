f96b99c99e85e88c592d2a955365c469e6b45052
elmarkazumov <elmarkazumov20@gmail.com>
2025-07-29 23:33:54 +0500

    fix overflow hidden

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -14,7 +14,7 @@ export default function Task({text, description, completed, onToggle, onStartEdi
     return (
         <div className="min-w-[400px] min-h-[150px] flex flex-col justify-center box-border p-3.5 rounded-2xl bg-gray-100">
             <div className="h-20 flex justify-between items-center">
-                <div className="w-11/12 h-full cursor-pointer" onDoubleClick={onStartEdit}>
+                <div className="w-11/12 h-full cursor-pointer overflow-hidden" onDoubleClick={onStartEdit}>
                     <p className={`w-full text-2xl ${!description ? `break-all`: `overflow-hidden overflow-ellipsis`}`}>{text}</p>
                     <p className="overflow-hidden overflow-ellipsis text-gray-700">{description}</p>
                 </div>
